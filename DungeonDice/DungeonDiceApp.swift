42ddca549dd875a11d3bffd39081aa89182c1cf8
Stephanie Villalobos <svillalobos2027@gmail.com>
2026-02-04 11:23:59 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by VILLALOBOS, STEPHANIE on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
